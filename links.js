df631713d23faa263ba6f0865be734be17f5eb32
MGHY <2970659912@qq.com>
2025-06-19 14:33:48 +0800

    初始提交：添加项目基础文件

---
+++ links.js
@@ -0,0 +1,145 @@
+const linksData = [
+  // {
+  //   category: "搜索引擎",
+  //   name: "百度",
+  //   url: "/Web_page_collection/sj.html",
+  //   mainDesc: "百度一下，你就知道",
+  //   subDesc: "中文搜索引擎",
+  //   // localIcon: "icons/baidu.ico"
+  // },
+  // {
+  //   category: "搜索引擎",
+  //   name: "Google",
+  //   url: "https://www.google.com",
+  //   mainDesc: "Google 搜索",
+  //   subDesc: "全球搜索引擎",
+  // },
+  // {
+  //   category: "开发工具",
+  //   name: "GitHub",
+  //   url: "https://github.com",
+  //   mainDesc: "代码托管平台",
+  //   subDesc: "开发者社区",
+  // },
+  // {
+  //   category: "开发工具",
+  //   name: "MDN",
+  //   url: "https://developer.mozilla.org",
+  //   mainDesc: "Web 开发文档",
+  //   subDesc: "权威技术文档",
+  // },
+  // {
+  //   category: "社交媒体",
+  //   name: "微博",
+  //   url: "https://weibo.com",
+  //   mainDesc: "新浪微博",
+  //   subDesc: "社交网络平台",
+  // },
+  // {
+  //   category: "视频平台",
+  //   name: "哔哩哔哩",
+  //   url: "https://www.bilibili.com",
+  //   mainDesc: "B站",
+  //   subDesc: "视频弹幕网站",
+  // },
+  {
+    category: "工作",
+    name: "工时记录表",
+    url: "./工时记录表/gs.html",
+    mainDesc: "",
+    subDesc: "工资与加班管理系统",
+    localIcon: "icons/gs.ico",
+  },
+  {
+    category: "工作",
+    name: "工资计算器",
+    url: "./工资计算器/hwgz.html",
+    mainDesc: "",
+    subDesc: "工资计算器",
+    localIcon: "./icons/gz.ico",
+  },
+  {
+    category: "工作",
+    name: "在线计时器",
+    url: "./在线计时器/在线计时器.html",
+    mainDesc: "",
+    subDesc: "在线/离线计时器",
+    localIcon: "icons/jsq.ico",
+  },
+  // AI
+  {
+    category: "AI工具",
+    name: "AI-NextChat",
+    url: "https://ai.mghy.top/",
+    mainDesc: "",
+    subDesc: "在线AI网页",
+    icon: "❌",
+  },
+  {
+    category: "个人",
+    name: "我的破博客",
+    url: "https://mghy.top/",
+    mainDesc: "",
+    subDesc: "我的小站Blog",
+    localIcon: "https://vcl.mghy.top/img/favicon.ico",
+    icon: "❌"
+  },
+  {
+    category: "个人",
+    name: "个人主页",
+    url: "https://zy.mghy.top/",
+    mainDesc: "",
+    subDesc: "我的个人页",
+    localIcon: "https://vcl.mghy.top/img/favicon.ico",
+    icon: "❌",
+  },
+  {
+    category: "个人",
+    name: "图床",
+    url: "https://tu.mghy.top/",
+    mainDesc: "",
+    subDesc: "Telegraph-Image",
+    icon: "❌",
+  },
+  {
+    category: "个人",
+    name: "我的Docs文档库",
+    url: "https://docs.mghy.top/",
+    mainDesc: "",
+    subDesc: "好记性不如烂笔头",
+    localIcon: "https://vcl.mghy.top/img/favicon.ico",
+    icon: "❌",
+  },
+  {
+    category: "个人",
+    name: "待办Todo",
+    url: "https://todo.mghy.top/",
+    mainDesc: "",
+    subDesc: "完成了今天的待办了吗?",
+    localIcon: "https://vcl.mghy.top/img/favicon.ico",
+    icon: "❌",
+  },
+
+
+  // 测试用例1 - 错误链接
+  // {
+  //   category: "测试",
+  //   name: "错误链接",
+  //   url: "https://错误的.链接/",
+  //   icon: "❌"
+  // },
+
+  // // 测试用例2 - 无icon属性
+  // {
+  //   category: "测试",
+  //   name: "无备用图标",
+  //   url: "https://不存在.com/"
+  // },
+
+  // // 测试用例3 - 正常情况
+  // {
+  //   category: "测试",
+  //   name: "正常网站",
+  //   url: "https://www.google.com"
+  // }
+];
